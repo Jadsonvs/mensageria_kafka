f58f41b6555c6bdaa4f248a4451aa6a480cf9233
Jadsonvs <jadson.vsantos2013@gmail.com>
2023-07-18 22:48:57 -0300

    Paralelizando e a importância das keys

--- src/main/java/br/com/alura/ecommerce/NewOrderMain.java
+++ src/main/java/br/com/alura/ecommerce/NewOrderMain.java
@@ -7,6 +7,8 @@ import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
+import java.util.Random;
+import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 public class NewOrderMain {
@@ -22,23 +24,28 @@ public class NewOrderMain {
 
     //Criando um produtor e enviando a msg "132123,123456,09876512" no tópico "ECOMMERCE_NEW_ORDER"
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-       var producer = new KafkaProducer<String, String>(properties());//Criando o produtor e definindo propriedades
-       var value = "132123,123456,1234";//Mensagem que será enviada no tópico
-       var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", value, value);//Configurando o que será gravado no kafka
+        var producer = new KafkaProducer<String, String>(properties());//Criando o produtor e definindo propriedades
 
-        Callback callback = (data, ex) -> {
-            if (ex != null) {
-                ex.printStackTrace();
-                return;
-            }
-            System.out.println("sucesso enviando " + data.topic() + ":::partition " + data.partition() +
-                    "/ offfset " + data.offset() + "/ " + data.timestamp());
-        };
-        var email = "Thank you for your order! We are processing your order!";
-        var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", email, email);
-        producer.send(record, callback).get();
-       producer.send(emailRecord, callback).get();
+        for(var i = 0; i<100; i++) {
 
+            var key = UUID.randomUUID().toString();
+            var value = key + ",123456,1234";//Mensagem que será enviada no tópico
+            var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", key, value);//Configurando o que será gravado no kafka
+
+            var email = "Thank you for your order! We are processing your order!";
+            var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", key, email);
+
+            Callback callback = (data, ex) -> {
+                if (ex != null) {
+                    ex.printStackTrace();
+                    return;
+                }
+                System.out.println("sucesso enviando " + data.topic() + ":::partition " + data.partition() +
+                        "/ offfset " + data.offset() + "/ " + data.timestamp());
+            };
+            producer.send(record, callback).get();
+            producer.send(emailRecord, callback).get();
+        }
     }
 
 }
