cede268f1a0e19f246860a51bea0b38e08deb6b8
Jadsonvs <jadson.vsantos2013@gmail.com>
2023-08-20 01:23:59 -0300

    Serviços que acessa bancos externos

--- common-kafka/src/main/java/br/com/alura/ecommerce/KafkaService.java
+++ common-kafka/src/main/java/br/com/alura/ecommerce/KafkaService.java
@@ -13,40 +13,37 @@ import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 import java.util.regex.Pattern;
 
-class KafkaService<T> implements Closeable {
+public class KafkaService<T> implements Closeable {
     private final KafkaConsumer<String, T> consumer;
     private final ConsumerFunction parse;
 
-     KafkaService(String groupId, String topic, ConsumerFunction parse, Class<T> type, Map<String, String> properties) {
+    public KafkaService(String groupId, String topic, ConsumerFunction parse, Class<T> type, Map<String, String> properties) {
         this(parse, groupId, type, properties);
-        consumer.subscribe(Collections.singleton(topic));
+        consumer.subscribe(Collections.singletonList(topic));
     }
 
-    public KafkaService(String groupId, Pattern topic, ConsumerFunction parse, Class<T> type, Map<String, String> properties) {
-        this(parse, groupId,type, properties);
+    public KafkaService(String groupId, Pattern topic, ConsumerFunction parse, Class<T> type, Map<String,String> properties) {
+        this(parse, groupId, type, properties);
         consumer.subscribe(topic);
     }
 
-    private KafkaService(ConsumerFunction parse, String groupId, Class<T> type, Map<String, String> properties) {
+    public KafkaService(ConsumerFunction parse, String groupId, Class<T> type, Map<String, String> properties) {
         this.parse = parse;
         this.consumer = new KafkaConsumer<>(getProperties(type, groupId, properties));
     }
 
-
-    void run() {
+   public void run() {
         while (true) {
             var records = consumer.poll(Duration.ofMillis(100));
-
             if (!records.isEmpty()) {
                 System.out.println("Encontrei " + records.count() + " registros");
                 for (var record : records) {
                     try {
                         parse.consume(record);
-                    } catch (ExecutionException e) {
-                     //so far, just logging the exception for this
-                        e.printStackTrace();
-                    } catch (InterruptedException e) {
-                        //so far, just logging the exception for this
+                    } catch (Exception e) {
+                        // only catches Exception because no matter which Exception
+                        // i want to recover and parse the next one
+                        // so far, just logging the exception for this message
                         e.printStackTrace();
                     }
                 }
@@ -54,17 +51,17 @@ class KafkaService<T> implements Closeable {
         }
     }
 
-    private Properties getProperties(Class<T> type, String groupId, Map<String, String> OverrideProperties) {
+    private Properties getProperties(Class<T> type, String groupId, Map<String, String> overrideProperties) {
         var properties = new Properties();
         properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
         properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, GsonDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
         properties.setProperty(ConsumerConfig.CLIENT_ID_CONFIG, UUID.randomUUID().toString());
-        properties.setProperty(GsonDeserializer.TYPE_CONFIG,type.getName());
-        properties.putAll(OverrideProperties);
+        properties.setProperty(GsonDeserializer.TYPE_CONFIG, type.getName());
+        properties.putAll(overrideProperties);
         return properties;
-    };
+    }
 
     @Override
     public void close() {
