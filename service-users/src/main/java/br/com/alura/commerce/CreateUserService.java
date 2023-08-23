93749400fff08524df07f06b0ace2485209c0841
Jadsonvs <jadson.vsantos2013@gmail.com>
2023-08-22 22:43:25 -0300

    Evoluindo servicos, schemas e escolhendo id adequado

--- service-users/src/main/java/br/com/alura/commerce/CreateUserService.java
+++ service-users/src/main/java/br/com/alura/commerce/CreateUserService.java
@@ -7,15 +7,23 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Map;
+import java.util.UUID;
+
 public class CreateUserService {
     private final Connection connection;
 
     //Criando conexão com o banco e criando tabela
     CreateUserService() throws SQLException {
         String url = "jdbc:sqlite:target/users_database.db";
-        this.connection = DriverManager.getConnection(url);
-        connection.createStatement().execute("create table Users (" +
-                "uuid varchar(200) primary key," + "email varchar(200))");
+        connection = DriverManager.getConnection(url);
+        try {
+            connection.createStatement().execute("create table Users (" +
+                    "uuid varchar(200) primary key," +
+                    "email varchar(200))");
+        } catch (SQLException ex) {
+            //be carefull, the sql could be wrong, be really carefull
+            ex.printStackTrace();
+        }
     }
     public static void main(String[] args) throws SQLException {
         var createUserService = new CreateUserService();
@@ -38,15 +46,16 @@ public class CreateUserService {
     }
     //Inserindo valores nos atributos da tabela do banco
     private void insertNewUser(String email) throws SQLException {
-        var insert = connection.prepareStatement("insert into Users (uuid, email) values (?, ?)");
-        insert.setString(1, "uuid");
+        var insert = connection.prepareStatement("insert into Users (uuid, email) " +
+                "values (?,?)");
+        insert.setString(1, UUID.randomUUID().toString());
         insert.setString(2, email);
         insert.execute();
         System.out.println("Usuário uuid e " + email + " adicionado");
     }
     //Verificando se usuário existe dentro do banco com uma consulta
     private boolean isNewEmail(String email) throws SQLException {
-        var exists = connection.prepareStatement("select from Users where email = ? limit 1");
+        var exists = connection.prepareStatement("select * from Users where email = ? limit 1");
         exists.setString(1, email);
         var results = exists.executeQuery();
         return !results.next();
--- service-users/src/main/java/br/com/alura/commerce/Main.java
+++
@@ -1,7 +0,0 @@
-package br.com.alura.commerce;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
