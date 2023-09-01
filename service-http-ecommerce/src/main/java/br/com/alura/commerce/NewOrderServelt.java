cd26be7affd32e8b9c128d40c26a0867c0bba89d
Jadsonvs <jadson.vsantos2013@gmail.com>
2023-09-01 08:55:58 -0300

    MMicro servico de email e fast delegate real

--- service-http-ecommerce/src/main/java/br/com/alura/commerce/NewOrderServelt.java
+++ service-http-ecommerce/src/main/java/br/com/alura/commerce/NewOrderServelt.java
@@ -13,12 +13,10 @@ import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 public class NewOrderServelt extends HttpServlet {
     private final KafkaDispatcher<Order> orderDispatcher = new KafkaDispatcher<>();
-    private final KafkaDispatcher<String> emailDispatcher = new KafkaDispatcher<>();
     @Override
     public void destroy() {
         super.destroy();
         orderDispatcher.close();
-        emailDispatcher.close();
     }
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -34,11 +32,6 @@ public class NewOrderServelt extends HttpServlet {
             orderDispatcher.send("ECOMMERCE_NEW_ORDER", email,
                     new CorrelationId(NewOrderServelt.class.getSimpleName()),
                     order);
-
-            var emailCode = "Thank you for your order! We are processing your order!";
-            emailDispatcher.send("ECOMMERCE_SEND_EMAIL", email,
-                    new CorrelationId(NewOrderServelt.class.getSimpleName()),
-                    emailCode);
 
             System.out.println("New order sent successfully.");
             resp.setStatus(HttpServletResponse.SC_ACCEPTED);
