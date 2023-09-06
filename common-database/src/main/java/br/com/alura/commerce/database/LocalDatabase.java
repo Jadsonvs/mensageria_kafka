db9c4b99017703aae9644c44c42c6bc7a5b0347d
Jadsonvs <jadson.vsantos2013@gmail.com>
2023-09-05 22:54:55 -0300

    Extraindo banco de dados para o módulo common-database

---
+++ common-database/src/main/java/br/com/alura/commerce/database/LocalDatabase.java
@@ -0,0 +1,36 @@
+package br.com.alura.commerce.database;
+
+import java.sql.*;
+public class LocalDatabase {
+    private final Connection connection;
+
+    //Criando conexão com o banco e criando tabela
+    public LocalDatabase(String name) throws SQLException {
+        String url = "jdbc:sqlite:target/" + name + ".db";
+        connection = DriverManager.getConnection(url);
+    }
+
+    //yes, this is way too generic.
+    //according to your database tool, avoid injection.
+    public void createIfNotExists(String sql) {
+        try {
+            connection.createStatement().execute(sql);
+        } catch (SQLException ex) {
+            //be carefull, the sql could be wrong, be really carefull
+            ex.printStackTrace();
+        }
+    }
+    public void update(String statement, String ... params) throws SQLException {
+        prepare(statement, params).execute();
+    }
+    private PreparedStatement prepare(String statement, String[] params) throws SQLException {
+        var preparedStatement = connection.prepareStatement(statement);
+        for(int i = 0; i < params.length; i++) {
+            preparedStatement.setString(i + 1, params[i]);
+        }
+        return preparedStatement;
+    }
+    public ResultSet query(String query, String ... params) throws SQLException {
+        return prepare(query, params).executeQuery();
+    }
+}
--- service-users/src/main/java/br/com/alura/commerce/CreateUserService.java
+++ service-users/src/main/java/br/com/alura/commerce/CreateUserService.java
@@ -1,29 +1,21 @@
 package br.com.alura.commerce;
 
+import br.com.alura.commerce.database.LocalDatabase;
 import br.com.alura.ecommerce.Message;
 import br.com.alura.ecommerce.consumer.ConsumerService;
 import br.com.alura.ecommerce.consumer.ServiceRunner;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.UUID;
 public class CreateUserService implements ConsumerService<Order> {
-    private final Connection connection;
-
-    //Criando conexão com o banco e criando tabela
-    private CreateUserService() throws SQLException {
-        String url = "jdbc:sqlite:target/users_database.db";
-        connection = DriverManager.getConnection(url);
-        try {
-            connection.createStatement().execute("create table Users (" +
-                    "uuid varchar(200) primary key," +
-                    "email varchar(200))");
-        } catch (SQLException ex) {
-            //be carefull, the sql could be wrong, be really carefull
-            ex.printStackTrace();
-        }
+    private final LocalDatabase database;
+    CreateUserService() throws SQLException {
+        this.database = new LocalDatabase("users_database");
+        this.database.createIfNotExists(
+                "create table Users (" +
+                "uuid varchar(200) primary key," +
+               "email varchar(200))");
     }
     public static void main(String[] args) {
         new ServiceRunner<>(CreateUserService::new).start(1);
@@ -48,19 +40,13 @@ public class CreateUserService implements ConsumerService<Order> {
 
     //Inserindo valores nos atributos da tabela do banco
     private void insertNewUser(String email) throws SQLException {
-        var insert = connection.prepareStatement("insert into Users (uuid, email) " +
-                "values (?,?)");
         var uuid = UUID.randomUUID().toString();
-        insert.setString(1, uuid);
-        insert.setString(2, email);
-        insert.execute();
-        System.out.println("Usuário " + uuid + " e " + email + " adicionado");
+        database.update("insert into Users (uuid, email) " +
+                "values (?,?)", uuid, email);
     }
     //Verificando se usuário existe dentro do banco com uma consulta
     private boolean isNewEmail(String email) throws SQLException {
-        var exists = connection.prepareStatement("select * from Users where email = ? limit 1");
-        exists.setString(1, email);
-        var results = exists.executeQuery();
+       var results = database.query("select uuid from Users where email = ? limit 1", email);
         return !results.next();
     }
 }
